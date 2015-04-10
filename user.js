2765a2d544529645224217cad2ccefea653a28a3
Dan Kahle <dan.kahle@gmail.com>
2015-04-10 08:23:52 -0700

    username/pass working

---
+++ user.js
@@ -0,0 +1,45 @@
+
+var mongoose = require('mongoose');
+var config = require('./config');
+var bcrypt = require('bcryptjs');
+
+var userSchema = new mongoose.Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String, select: false },
+  displayName: String,
+  picture: String,
+  facebook: String,
+  foursquare: String,
+  google: String,
+  github: String,
+  linkedin: String,
+  live: String,
+  yahoo: String,
+  twitter: String
+});
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(password, done) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    done(err, isMatch);
+  });
+};
+
+module.exports = mongoose.model('User', userSchema);
+
+mongoose.connect(config.MONGO_URI);
+mongoose.connection.on('error', function() {
+  console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
+});
